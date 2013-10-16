6173d8260953e087b653c1347c676ee009541cee
Erik Steen <erik.steen@bellevuecollege.edu>
2013-10-16 11:15:42 -0700

    Task #4385 - Provide UI for audio segments to appear on multiple programs on multiple days
    This also adds the WPAlchemy class to the project. WPAlchemy
    is a class that makes using WordPress Metaboxes really easy.

---
+++ metaboxes/simple-spec.php
@@ -0,0 +1,32 @@
+<?php
+
+$custom_metabox = $simple_mb = new WPAlchemy_MetaBox(array
+(
+	'id' => '_custom_meta',
+	'title' => 'My Custom Meta',
+	'types' => array(''),
+	'template' => get_stylesheet_directory() . '/metaboxes/simple-meta.php',
+));
+
+$audio_metabox = $simple_mb = new WPAlchemy_MetaBox(array
+(
+	'id' => '_audio_meta',
+	'title' => 'Air Dates',
+	'template' => get_stylesheet_directory() . '/metaboxes/audio-meta.php',
+	'types' => array('audio'),
+	'save_action'   => 'save_taxonomy_terms',
+	'save_action'   => 'my_save_action_func',
+	//'save_action'   => 'save_degree_title',
+	'mode' => WPALCHEMY_MODE_EXTRACT,
+));
+
+function save_taxonomy_terms($meta, $post_id) {
+	wp_set_post_terms($post_id, array($meta['my_terms']), 'category', FALSE);
+}
+
+function my_save_action_func ($meta, $post_id) {
+	$value = $meta['program_terms'];
+	update_post_meta($post_id, '_my_custom_title3_',  $value);
+}
+
+/* eof */
