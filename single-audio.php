4e799048dba7135df948ad7b124e47ce94463d01
Erik Steen <erik.steen@bellevuecollege.edu>
2013-11-18 13:25:01 -0800

    Simply single-audio page template

--- single-audio.php
+++ single-audio.php
@@ -1,50 +1,18 @@
-<?php
+<?php get_header(); ?>
 
-get_header(); ?>
+<div class="whatpageisthis">single-audio.php</div>		
 
 	<div class="container">
-	<?php while (have_posts() ) : the_post(); ?>
-<?php the_content(); ?>
-<h2>Display Test</h2>
-<?php
+        <div class="row">	
+            <div class="span8" id="content">
 
-$audios = get_posts(array(
-	'numberposts' => -1,
-	'post_type' => 'audio',
-	'suppress_filters' => false,
-	'orderby' => 'title',
-	'order' => 'ASC'
-));
+				<?php while (have_posts() ) : the_post(); ?>
+					<h2><?php the_title(); ?></h2>
+					<?php the_content(); ?>		
+				<?php endwhile; ?>
 
-if($audios) { ?>
-		<ol><?php
-
-    foreach($audios as $audio) { 
-	        global $audio_metabox;
-	        $id = $audio->ID;
-	        //$metaKey = $audio_metabox->id;
-	        //$program_terms = get_the_term_list( $audio->ID, 'programs', '', ', ', '' );
-	//        error_log("value :".$program_terms);
-
-			?>
-			<?php /*
-			<h3><?php 
-$taxonomies=get_taxonomies('','names'); 
-foreach ($taxonomies as $taxonomy ) {
-  echo '<p>'. $taxonomy. '</p>';
-}
-?></h3>
-*/ ?>
-			<li>
-				<?php echo $id . ' - ' ;?>
-				<?php echo get_the_term_list( $post->ID, 'programs'); ?><br />
-				Title: <?php var_dump(get_the_title( $audio->ID )); ?>
-			</li><?php
-		}
-}
-//$audio_metabox->the_field('air_date');
-//$audio_metabox->the_value();
-			?></ol>
-<?php endwhile; ?>
-	</div>
+			</div><!--#content .span8 -->
+			<?php get_sidebar(); ?>
+		</div><!-- row -->
+	</div><!-- container -->
 <?php get_footer(); ?>
