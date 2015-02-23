4a2f7ae41c0592cf6a10e914656961166f164c46
Jonathan Windle <jonathan.windle@bellevuecollege.edu>
2015-02-23 12:31:57 -0800

    Clean postData Function
    The postData function contains double indentation and many echo lines
    that are commented out.

--- inc/currentprograms.php
+++ inc/currentprograms.php
@@ -35,109 +35,95 @@ function getMetaKey($day)
 
 
 
-function postData($day_meta_key,&$currentPostId, &$lastPostId, &$futurePostId)
-	{
-		global $wpdb;
-		$sql = "SELECT wp.post_id,wp.meta_value as starttime, wpp2.meta_value as endtime , wpo.post_title FROM `wp_postmeta` as wp,wp_postmeta as wpp2, wp_posts as wpo WHERE
-				wpo.id=wp.post_id and
-				(wp.meta_key='onair_starttime') and
-				wp.post_id in 
-				(
-					select wpp.post_id from wp_postmeta as wpp where wpp.meta_key='$day_meta_key'
-				) and
-        		(wpp2.meta_key = 'onair_endtime' and wpp2.post_id = wp.post_id)	
-				
-				";
+function postData( $day_meta_key, &$currentPostId, &$lastPostId, &$futurePostId
+) {
+	global $wpdb;
+	$sql = 'SELECT wp.post_id, ' .
+		'wp.meta_value as starttime, ' .
+		'wpp2.meta_value as endtime, ' .
+		'wpo.post_title ' .
+		'FROM `wp_postmeta` AS wp, wp_postmeta AS wpp2, wp_posts AS wpo ' .
+		"WHERE wpo.id=wp.post_id AND (wp.meta_key='onair_starttime') AND " .
+		'wp.post_id in ' .
+			'(SELECT wpp.post_id FROM ' .
+			"wp_postmeta AS wpp WHERE wpp.meta_key='$day_meta_key') " .
+		'AND ' .
+			"(wpp2.meta_key = 'onair_endtime' AND wpp2.post_id = wp.post_id)";
 
-		$post_data = $wpdb->get_results( $sql);
-		$currentPost_starttime = "";
-		$currentPost_endtime = "";
-		$currentTime = strtotime(date('G:i'));
-		//$currentTime = strtotime("9:30");
-		$midnight = strtotime("00:00");// its always the least 
-		//echo "\ncurrent time:".$currentTime;
-		for($i=0;$i<count($post_data);$i++)
+	$post_data = $wpdb->get_results( $sql );
+	$currentPost_starttime = '';
+	$currentPost_endtime = '';
+	$currentTime = strtotime( date( 'G:i' ) );
+	$midnight = strtotime( '00:00' ); // its always the least
+	for ( $i = 0; $i < count( $post_data ); $i++ ) {
+		$postId = $post_data[ $i ]->post_id;
+		$startTime = strtotime( $post_data[ $i ]->starttime );
+		$endTime = strtotime( $post_data[ $i ]->endtime );
+		$postTitle = $post_data[ $i ]->post_title;
+		if ( ( $currentTime >= $startTime && $currentTime <= $endTime )
+			|| ( $currentTime >= $startTime && $endTime == $midnight ) )
 		{
-			$postId = $post_data[$i]->post_id;
-			$startTime = strtotime($post_data[$i]->starttime);
-			//echo "\n post id:".$postId;
-			//echo "\n start time:".$startTime;
-			$endTime = strtotime($post_data[$i]->endtime);
-			//echo "\n end time :".$endTime;
-			$postTitle = $post_data[$i]->post_title;
-			if(($currentTime>=$startTime && $currentTime <= $endTime) || ($currentTime>=$startTime && $endTime == $midnight))
-			{
-				//This is the current post playing
-				//echo "Current Post title :".$postTitle;
-
-				$currentPostId = $postId ;
-				//echo "current post id:".$currentPostId;
-				$currentPost_starttime = $startTime;
-				$currentPost_endtime = $endTime;
-				break;
-			}
-			//else if()
+			$currentPostId = $postId;
+			$currentPost_starttime = $startTime;
+			$currentPost_endtime = $endTime;
+			break;
 		}
-		
-		$flag_nextday = 0;
-		$flag_pastday = 0;
-		
-
-		if($currentPost_endtime == $midnight)//For future post falling on next day
-		{
-			$next_day = strtolower(date("l",strtotime("tomorrow")));
-			//echo $next_day;
-			$meta_key = getMetaKey($next_day);
-			$sql = "select wpp.post_id,wpp.meta_value from wp_postmeta as wpp where wpp.meta_key = 'onair_starttime' and
-			 wpp.post_id in (select wp.post_id from wp_postmeta as wp where wp.meta_key = '$meta_key')  order by wpp.meta_value ASC limit 1";
-			 $next_post = $wpdb->get_results( $sql);
-			 $futurePostId = $next_post[0]-> post_id;
-			 //echo "Future post:".$futurePostId;
-			 $flag_nextday = 1;
-		}
-
-		if($currentPost_starttime == $midnight)//For past post falling on previous day
-		{
-			$previous_day = strtolower(date("l",strtotime("yesterday")));
-			$meta_key = getMetaKey($previous_day);
-			$sql = "select wpp.post_id,wpp.meta_value from wp_postmeta as wpp where wpp.meta_key = 'onair_starttime' and
-			 wpp.post_id in (select wp.post_id from wp_postmeta as wp where wp.meta_key = '$meta_key')  order by wpp.meta_value DESC limit 1";
-			 $last_post = $wpdb->get_results( $sql);
-			 $lastPostId = $last_post[0]-> post_id;
-			 //echo "Past post id:".$lastPostId;
-			 $flag_pastday = 1;
-		}
-		//echo "flag past :".$flag_pastday;
-		//echo "future flag:".$flag_nextday;
-		
-		for($i=0;$i<count($post_data);$i++)
-		{
-			$postId = $post_data[$i]->post_id;
-			$startTime = strtotime($post_data[$i]->starttime);
-			$endTime = strtotime($post_data[$i]->endtime);
-			$postTitle = $post_data[$i]->post_title;
-			if($endTime == $currentPost_starttime && $flag_pastday == 0) // Last post
-			{
-				$lastPostId = $postId;
-				//echo "Past post id:".$lastPostId;
-			}
-
-			if($currentPost_endtime == $startTime &&  $flag_nextday == 0)//Future post
-			{
-				$futurePostId = $postId;
-				//echo "Future post:".$futurePostId;
-			}
-			
-			
-		}
-		//return $myrows;
 	}
-	
-	
-	
-	
-	
-	
+
+	$flag_nextday = 0;
+	$flag_pastday = 0;
+
+	//For future post falling on next day
+	if ( $currentPost_endtime == $midnight ) {
+		$next_day = strtolower( date( 'l', strtotime( 'tomorrow' ) ) );
+		$meta_key = getMetaKey( $next_day );
+		$sql = 'SELECT wpp.post_id, wpp.meta_value ' .
+			'FROM wp_postmeta AS wpp ' .
+			"WHERE wpp.meta_key = 'onair_starttime' " .
+			'AND wpp.post_id IN ' .
+				'(SELECT wp.post_id ' .
+				'FROM wp_postmeta AS wp ' .
+				"WHERE wp.meta_key = '$meta_key')" .
+			'ORDER BY wpp.meta_value ASC limit 1';
+		$next_post = $wpdb->get_results( $sql );
+		$futurePostId = $next_post[0]->post_id;
+		$flag_nextday = 1;
+	}
+
+	//For past post falling on previous day
+	if ( $currentPost_starttime == $midnight ) {
+		$previous_day = strtolower( date( 'l', strtotime( 'yesterday' ) ) );
+		$meta_key = getMetaKey( $previous_day );
+		$sql = 'SELECT wpp.post_id, wpp.meta_value ' .
+			'FROM wp_postmeta AS wpp ' .
+			"WHERE wpp.meta_key = 'onair_starttime' " .
+			'AND wpp.post_id IN ' .
+				'(SELECT wp.post_id ' .
+				'FROM wp_postmeta AS wp ' .
+				"WHERE wp.meta_key = '$meta_key')" .
+			'ORDER BY wpp.meta_value DESC limit 1';
+		$last_post = $wpdb->get_results( $sql );
+		$lastPostId = $last_post[0]->post_id;
+		$flag_pastday = 1;
+	}
+
+	for ( $i = 0; $i < count( $post_data ); $i++ ) {
+		$postId = $post_data[ $i ]->post_id;
+		$startTime = strtotime( $post_data[ $i ]->starttime );
+		$endTime = strtotime( $post_data[ $i ]->endtime );
+		$postTitle = $post_data[ $i ]->post_title;
+
+		// Last post
+		if ( $endTime == $currentPost_starttime && $flag_pastday == 0 ) {
+			$lastPostId = $postId;
+		}
+
+		//Future post
+		if ( $currentPost_endtime == $startTime && $flag_nextday == 0 ) {
+			$futurePostId = $postId;
+		}
+	}
+}
 	
 	function airTimeForGivenPost($postid)
 	{
