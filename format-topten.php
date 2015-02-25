9efe164cede0e4feadbffe13cb6794c2aeb174cb
Erik Steen <erik.steen@bellevuecollege.edu>
2015-02-25 07:23:02 -0800

    Fix home page excerpt issue on posts using top-ten template
    [Bug] Page break not working on home page excerpt of
     posts with bullets or numbers. This had nothing to
    do with numbers/bullets. The issue was that the page
    template format-topten.php wasn't displaying the_excerpt
    it was displaying the_content.

--- format-topten.php
+++ format-topten.php
@@ -10,25 +10,24 @@
 				if(get_post(get_post_thumbnail_id())->post_excerpt) { ?>
 					<span class="featured-caption media-object"><?php echo get_post( get_post_thumbnail_id() )->post_excerpt ?></span>
 				<?php } ?>
-		<?php    					
+	<?php    					
 		}
 		else {
 			//echo '<img src="' . get_bloginfo( 'stylesheet_directory' ) . '/img/thumbnail-default.png" />';
 		}
-	?>												    
-	    
+	?>
 	    </a>
-	
+
 	<div class="media-body">
 
 		<div class="media-content">
 		<p><small><?php the_time('F j, Y'); ?> - <?php the_time('g:i a'); ?></small></p>
-			<?php the_content(); ?>
+			<?php the_excerpt(); ?>
 		</div><!-- media-content -->
 	    <?php 
 		if (is_single($post)){
 		?> 
-            
+
         <?php
 		} else {
 		?> 
@@ -37,7 +36,7 @@
             </a>
             </p>
         <?php	
-			
+
 		}
 		?>
     </div><!-- media-body -->
