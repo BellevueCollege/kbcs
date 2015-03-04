18d505ff19ae1a154e05166776e2248c97b84779
Jonathan Windle <jonathan.windle@bellevuecollege.edu>
2015-03-03 16:07:32 -0800

    Normalize code in format-standard.php
    Normalize code in format-standard.php to have consistent tabbing and
    style.

--- format-standard.php
+++ format-standard.php
@@ -1,43 +1,28 @@
-<h2 <?php post_class() ?>>							
-	<a href="<?php the_permalink(); ?>"><?php the_title();?></a>
-</h2>
-
+<h2 <?php post_class() ?>><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
 <div class="media">
-    <a class="pull-left" href="<?php the_permalink(); ?>">
-	<?php 
+	<a class="pull-left" href="<?php the_permalink(); ?>">
+		<?php
 		if ( has_post_thumbnail() ) {
-			the_post_thumbnail('thumbnail', array('class' => 'media-object'));
-				if(get_post(get_post_thumbnail_id())->post_excerpt) { ?>
-					<span class="featured-caption media-object"><?php echo get_post( get_post_thumbnail_id() )->post_excerpt ?></span>
-				<?php } ?>
-	<?php    					
-		}
-		else {
-			//echo '<img src="' . get_bloginfo( 'stylesheet_directory' ) . '/img/thumbnail-default.png" />';
-		}
-	?>												    
-	    </a>
-
-	<div class="media-body">
-
-		<div class="media-content">
-		<p><small><?php the_time('F j, Y'); ?> - <?php the_time('g:i a'); ?></small></p>
-			<?php the_excerpt(); ?>
-		</div><!-- media-content -->
-	    <?php 
-		if (is_single($post)){
-		?> 
-
-        <?php
-		} else {
-		?> 
-           <p> <a class="btn btn-small primary-read-more" href="<?php the_permalink(); ?>">
-                Read More <i class="icon-chevron-right"></i>
-            </a>
-            </p>
-        <?php	
-
+			the_post_thumbnail( 'thumbnail', array( 'class' => 'media-object' ) );
+			if( get_post( get_post_thumbnail_id() )->post_excerpt ) {
+		?>
+		<span class="featured-caption media-object"><?php echo get_post( get_post_thumbnail_id() )->post_excerpt ?></span>
+		<?php
+			}
 		}
 		?>
-    </div><!-- media-body -->
-</div><!-- media -->
+	</a>
+	<div class="media-body">
+		<div class="media-content">
+			<p><small><?php the_time( 'F j, Y' ); ?> - <?php the_time( 'g:i a' ); ?></small></p>
+			<?php the_excerpt(); ?>
+		</div>
+		<?php
+		if ( is_single( $post ) ) {
+		} else { ?>
+		<p><a class="btn btn-small primary-read-more" href="<?php the_permalink(); ?>">
+			Read More <i class="icon-chevron-right"></i>
+		</a></p>
+		<?php } ?>
+	</div>
+</div>
