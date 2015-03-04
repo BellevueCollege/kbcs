c5bf71c0f569979fbe45497a707a9d363d16591c
Jonathan Windle <jonathan.windle@bellevuecollege.edu>
2015-03-04 13:46:46 -0800

    Remove custom_wp_trim_excerpt filter from get_the_excerpt
    Remove the custom_wp_trim_excerpt filter from get_the_excerpt as
    WordPress should really know best as to how this is accomplished.

--- functions.php
+++ functions.php
@@ -88,46 +88,6 @@ function be_hidden_meta_boxes($hidden, $screen) {
 	return $hidden;
 }
 
-###############################################
-// Change how WP handles the_excerpt
-###############################################
-
-function custom_wp_trim_excerpt($text) {
-$raw_excerpt = $text;
-if ( '' == $text ) {
-    //Retrieve the post content. 
-    $text = get_the_content('');
- 
-    //Delete all shortcode tags from the content. 
-    $text = strip_shortcodes( $text );
- 
-    $text = apply_filters('the_content', $text);
-    $text = str_replace(']]>', ']]&gt;', $text);
-     
-    $allowed_tags = '<p>,<a>,<em>,<strong>,<img>,<ol><ul><li>'; /*** MODIFY THIS. Add the allowed HTML tags separated by a comma.***/
-    $text = strip_tags($text, $allowed_tags);
-     
-    $excerpt_word_count = 55; /*** MODIFY THIS. change the excerpt word count to any integer you like.***/
-    $excerpt_length = apply_filters('excerpt_length', $excerpt_word_count); 
-     
-    $excerpt_end = '[...]'; /*** MODIFY THIS. change the excerpt endind to something else.***/
-    $excerpt_more = apply_filters('excerpt_more', ' ' . $excerpt_end);
-     
-    $words = preg_split("/[\n\r\t ]+/", $text, $excerpt_length + 1, PREG_SPLIT_NO_EMPTY);
-    if ( count($words) > $excerpt_length ) {
-        array_pop($words);
-        $text = implode(' ', $words);
-        $text = $text . $excerpt_more;
-    } else {
-        $text = implode(' ', $words);
-    }
-}
-return apply_filters('wp_trim_excerpt', $text, $raw_excerpt);
-}
-remove_filter('get_the_excerpt', 'wp_trim_excerpt');
-add_filter('get_the_excerpt', 'custom_wp_trim_excerpt');
-
-
 ##############################
 // Enqueue Scripts & Styles
 ##############################
