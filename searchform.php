5442738f38229dd7455f1b54d61e22c9f1c929bf
Drew King <andrew.phillip.king@gmail.com>
2024-10-16 16:08:51 -0700

    Searchform.php  now has a role for search input.

--- searchform.php
+++ searchform.php
@@ -1,7 +1,7 @@
 <form id="search" class="" method="get" action="<?php echo esc_url( home_url( '/' ) ); ?>"> 
   <div class="input-append">
 		<span aria-hidden="true" data-icon="&#xf002;"></span>
-		<input label="Search" class="input-xlarge" type="text" name="s" value="<?php echo trim( get_search_query() ); ?>">
+		<input role="search" label="Search" class="input-xlarge" type="text" name="s" value="<?php echo trim( get_search_query() ); ?>"/>
 		<input type='hidden' name='post_type' value='programs,segments,staff,events,ads' />
 		<button label="button" id="searchsubmit" value="Search" type="submit" class="btn">Search</button>
   </div>
