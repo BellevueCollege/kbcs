5138601b1c9751ce7a06fe670a9d989b99640662
Drew King <andrew.phillip.king@gmail.com>
2024-10-16 15:20:12 -0700

    The body of the content is now in a <main> tag. Still working on getting blog posts to show up as <article>

--- front-page.php
+++ front-page.php
@@ -4,62 +4,63 @@ get_header();
 <div class="whatpageisthis">front-page.php</div>
 <div class="container">
 	<div class="row">
-		<div class="span8" id="content">
-		<div id="hero-onair">On air</div>
-			<div id="hero-block" class="loading">
-				<div class="row-fluid" id="hero-text-wrapper">
-					<div class="loading">Loading...</div>
-					<div class="span9" id="hero-text">
-						<div class="inner">
-							<h1 id="hero-title"></h1>
-							<p id="hero-host" class="hostedby"></p>
-							<p id="hero-airtimes" class="program-days-times"></p>
-							<ul id="hero-links">
-								<li>
-									<a href="https://www.radiorethink.com/tuner/?stationCode=kbcs&stream=hi" class="streamlive" onClick="gaplusu('send', 'event', 'Outbound', 'Homepage Jumbotron', 'Live Stream');">
-										<i class="icon-volume-up"></i>Listen live
-									</a>
-								</li>
-								<li>
-									<a href="<?php echo home_url(); ?>/live-playlist/">
-										<i class="icon-th-list"></i>View Playlist
-									</a>
-								</li>
-								<li>
-									<a id="hero-link" href="" title="">
-										<i class="icon-list-alt"></i>View Program page
-									</a>
-								</li>
-							</ul>
-						</div>
-					</div>
-				</div>
-				<div id="hero-image">
-					<img src="<?php echo get_stylesheet_directory_uri(); ?>/img/program-hero-generic.jpg" alt="photo of cds in KBCS library" />
-				</div>
-			</div>
-			<ul id="hero-past-future" class="loading">
-				<li id="hero-past">
-					<a id="hero-past-link" href="">
-						<span class="inner">
-							<em><span class="corner"></span>Last Show</em>
-							<span id="hero-past-time"></span>
-							<span id="hero-past-title"></span>
-						</span>
-					</a>
-				</li>
-				<li id="hero-future">
-					<a id="hero-future-link" href="">
-						<span class="inner">
-							<em><span class="corner"></span>Next Show</em>
-							<span id="hero-future-time"></span>
-							<span id="hero-future-title"></span>
-						</span>
-					</a>
-				</li>
-			</ul>
-			<p id="schedulelink"><a href="program/"><i class="icon-calendar"></i>Weekly Schedule</a></p>
-
+		<main class="span8" id="content">
+			<section label="On-Air">
+				<div id="hero-onair">On air</div>
+					<div id="hero-block" class="loading">
+						<div class="row-fluid" id="hero-text-wrapper">
+							<div class="loading">Loading...</div>
+							<div class="span9" id="hero-text">
+								<div class="inner">
+									<h1 id="hero-title"></h1>
+									<p id="hero-host" class="hostedby"></p>
+									<p id="hero-airtimes" class="program-days-times"></p>
+									<ul id="hero-links">
+										<li>
+											<a href="https://www.radiorethink.com/tuner/?stationCode=kbcs&stream=hi" class="streamlive" onClick="gaplusu('send', 'event', 'Outbound', 'Homepage Jumbotron', 'Live Stream');">
+												<i class="icon-volume-up"></i>Listen live
+											</a>
+										</li>
+										<li>
+											<a href="<?php echo home_url(); ?>/live-playlist/">
+												<i class="icon-th-list"></i>View Playlist
+											</a>
+										</li>
+										<li>
+											<a id="hero-link" href="" title="">
+												<i class="icon-list-alt"></i>View Program page
+											</a>
+										</li>
+									</ul>
+								</div> <!-- inner -->
+							</div> <!-- hero-text -->
+						</div> <!-- row-fluid -->
+						<div id="hero-image">
+							<img src="<?php echo get_stylesheet_directory_uri(); ?>/img/program-hero-generic.jpg" alt="photo of cds in KBCS library" />
+						</div> <!-- hero-image -->
+					</div> <!-- hero-block -->
+					<ul id="hero-past-future" class="loading">
+						<li id="hero-past">
+							<a id="hero-past-link" href="">
+								<span class="inner">
+									<em><span class="corner"></span>Last Show</em>
+									<span id="hero-past-time"></span>
+									<span id="hero-past-title"></span>
+								</span>
+							</a>
+						</li>
+						<li id="hero-future">
+							<a id="hero-future-link" href="">
+								<span class="inner">
+									<em><span class="corner"></span>Next Show</em>
+									<span id="hero-future-time"></span>
+									<span id="hero-future-title"></span>
+								</span>
+							</a>
+						</li>
+					</ul>
+					<p id="schedulelink"><a href="program/"><i class="icon-calendar"></i>Weekly Schedule</a></p>
+			</section> <!-- On-Air -->
 			<?php
 			$sticky = get_option( 'sticky_posts' );
 			$args = array(
@@ -76,8 +77,8 @@ get_header();
 
 			if ( $sticky ) {
 			?>
-			<h2 <?php post_class() ?>><a href="<?php the_permalink(); ?>"><?php the_title();?></a></h2>
-			<div class="media">
+			<article class="media"> <!-- TODO : Make this tag appear in rendered page -->
+				<h2 <?php post_class() ?>><a href="<?php the_permalink(); ?>"><?php the_title();?></a></h2>
 				<a class="pull-left" href="<?php the_permalink(); ?>">
 				<?php
 					if ( has_post_thumbnail() ) {
@@ -91,7 +92,7 @@ get_header();
 					<div class="media-content">
 						<p><small><?php the_time( 'F j, Y' ); ?> - <?php the_time( 'g:i a' ); ?></small></p>
 						<?php the_excerpt(); ?>
-					</div>
+					</div> <!-- media-content -->
 					<?php if ( ! is_single( $post ) ) { ?>
 					<p>
 						<a class="btn btn-small primary-read-more" href="<?php the_permalink(); ?>">
@@ -99,8 +100,9 @@ get_header();
 						</a>
 					</p>
 					<?php } ?>
-				</div>
-			</div>
+				</div> <!-- media-body -->
+			</article> <!-- media -->
+			
 			<?php
 			}
 			endwhile;
@@ -128,10 +130,10 @@ get_header();
 			endwhile;
 			wp_reset_postdata();
 			?>
-		</div>
+		</main> <!-- content -->
 		<?php get_sidebar(); ?>
-	</div>
-</div>
+	</div> <!-- row -->
+</div> <!-- container -->
 <script type="text/javascript">
 	jQuery(function() { jQuery("#hero-past-future .title").equalHeights(); });
 </script>
