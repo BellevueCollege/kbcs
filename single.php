d3dc95f1d09a18281e39126454e8580f87fb7b02
Drew King <andrew.phillip.king@gmail.com>
2024-11-25 09:42:39 -0800

    Removed read-more button.

--- single.php
+++ single.php
@@ -54,10 +54,6 @@ if ( has_post_format( 'quote' )) {
         <?php
 		} else {
 		?> 
-           <p> <a class="btn btn-small primary-read-more" href="<?php the_permalink(); ?>">
-                Read More <i class="icon-chevron-right"></i>
-            </a>
-            </p>
         <?php	
 			
 		}
