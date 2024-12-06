6e6264b5bdbffa31c234244fe8ac6912af10dbed
Drew King <andrew.phillip.king@gmail.com>
2024-12-06 08:50:17 -0800

    Formatting now only renders a media-left div if there is a thumbnail.

--- format-standard.php
+++ format-standard.php
@@ -1,18 +1,24 @@
 <article title="<?php the_title(); ?>">
-<h2 <?php post_class() ?>><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-<div class="media">
-	<div class="media-left">
+	<h2 <?php post_class() ?>><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+	<div class="media">
 		<?php
-		if ( has_post_thumbnail() ) {
-			the_post_thumbnail( 'thumbnail', array( 'class' => 'media-object' ) );
+		if ( has_post_thumbnail() ) { ?>
+			<div class="media-left"> 
+				<?php 
+				the_post_thumbnail( 'thumbnail', array( 'class' => 'media-object' ) );
+				?>
+			
+			<?php 
 			if( get_post( get_post_thumbnail_id() )->post_excerpt ) {
-		?>
+			?>
 		<span class="featured-caption media-object"><?php echo get_post( get_post_thumbnail_id() )->post_excerpt ?></span>
 		<?php
 			}
+		?>
+		</div>
+		<?php
 		}
 		?>
-	</div>
 		<div class="media-content">
 			<p><small><?php the_time( 'F j, Y' ); ?> - <?php the_time( 'g:i a' ); ?></small></p>
 			<?php
@@ -27,5 +33,5 @@
 			}
 			?>
 		</div>
-</div>
+	</div>
 </article>
