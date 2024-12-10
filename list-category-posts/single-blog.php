33ab196f60ea35b52814dd042de71e62bd00e6ed
Drew King <andrew.phillip.king@gmail.com>
2024-12-10 15:36:13 -0800

    Catlist now renders the way we want.

--- list-category-posts/single-blog.php
+++ list-category-posts/single-blog.php
@@ -18,9 +18,13 @@ while ( have_posts() ) : the_post();
 
     // Post container with or without thumbnail
     $lcp_display_output .= '<article id="post-' . get_the_ID() . '" class="lcp-post">';
+	$lcp_display_output .= $this->get_post_title($post, 'h2'); // Semantic post title
+
+	// Container
+	$lcp_display_output .= '<div class="media">';
     
     if ( has_post_thumbnail($post->ID) ) {
-        $lcp_display_output .= '<div class="media">';
+        $lcp_display_output .= '<div class="media-left">';
         $lcp_display_output .= $this->get_thumbnail($post, 'medium'); // Get thumbnail
         $lcp_display_output .= '</div>';
     } else {
@@ -28,8 +32,7 @@ while ( have_posts() ) : the_post();
     }
 
     // Post content container
-    $lcp_display_output .= '<div class="post-content">';
-    $lcp_display_output .= $this->get_post_title($post, 'h2'); // Semantic post title
+    $lcp_display_output .= '<div class="media-content">';
     
     // Meta information with proper ARIA-hidden for visual metadata
     $lcp_display_output .= '<div class="post-meta" aria-hidden="true">';
@@ -45,6 +48,7 @@ while ( have_posts() ) : the_post();
     $lcp_display_output .= '<div class="post-read-more">';
     $lcp_display_output .= $this->get_posts_morelink($post);
     $lcp_display_output .= '</div>';
+
     $lcp_display_output .= '</div>'; // End of post-content
 
     $lcp_display_output .= '</article>'; // End of lcp-post
